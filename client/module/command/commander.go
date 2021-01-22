99e0817abb7ed6ce7f03c829567b204691a7f0fc
Maka8ka <42055988+Maka8ka@users.noreply.github.com>
2021-01-22 09:14:20 +0800

    Delete commander.go

--- client/module/command/commander.go
+++
@@ -1,8 +0,0 @@
-package command
-
-
-type Commander interface {
-	Exec(args ...string) (int, string, error)
-	ExecAsync(stdout chan string, args ...string) int
-	ExecIgnoreResult(args ...string) error
-}
